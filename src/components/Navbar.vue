5aa7abec71e5079b2c35ef5b5812d0e4cde36a8b
Diego Bustamante <Diego Bustamante>
2022-01-30 23:12:47 -0300

    Avance Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,44 +1,131 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <nav class="flex items-center justify-between flex-wrap bg-cv-color p-6">
-    <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <img :src="profileImage" alt="ImagenPerfil" class="h-14 w-14 rounded-full px-auto mr-4">
-      <div class="hidden text-2xl font-semibold md:flex tracking-tight text-white">Diego Bustamante
-      </div>  
-    </div>
-    <Menu>
-        <MenuButton class="text-white inline-flex p-3 hover:bg-cv-hover rounded md:hidden ml-auto hover:text-white outline-none nav-toggler"
-        data-target="#navigation">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </MenuButton>
-        <div class="hidden md:block md:ml-6">
-          <div class="flex space-x-4">
-            <MenuItems>
-              <MenuItem>                
-              </MenuItem>
-            </MenuItems>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ close }">
+    <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="relative flex items-center justify-between h-16">
+        <div class="absolute inset-y-0 right-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span class="sr-only">Open main menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div
+          class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start"
+        >
+          <div class="flex-shrink-0 flex items-center">
+            <h1 class="text-white text-base font-medium">Diego Bustamante</h1>
+          </div>
+          <div class="hidden sm:block sm:ml-6">
+            <div class="flex space-x-4">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'px-3 py-2 rounded-md text-sm font-medium',
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              >
+            </div>
           </div>
         </div>
-    </Menu>    
-  </nav>
+        <div
+          class="absolute inset-y-0 left-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        >
+          <!-- Profile dropdown -->
+          <Menu as="div" class="ml-3 relative">
+            <div>
+              <MenuButton
+                class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              >
+                <span class="sr-only">Open user menu</span>
+                <img
+                  class="h-8 w-8 rounded-full"
+                  :src="profileImage"
+                  alt="profile image"
+                />
+              </MenuButton>
+            </div>
+            <transition
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95"
+            >
+            </transition>
+          </Menu>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block px-3 py-2 rounded-md text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
 <script>
-import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/vue/outline";
+
+const navigation = [
+  { name: "Sobre Mi", href: "#", current: true },
+  { name: "Skills", href: "#", current: false },
+  { name: "Proyectos", href: "#", current: false },
+  { name: "Contacto", href: "#", current: false },
+];
+
 export default {
-  name: 'Navbar',
-  props:['profileImage'],
+  props: ["profileImage"],
   components: {
-      Menu,
-      MenuButton,
-      MenuItems,
-      MenuItem,
-    },
-}
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    BellIcon,
+    MenuIcon,
+    XIcon,
+  },
+  setup() {
+    return {
+      navigation,
+    };
+  },
+};
 </script>
-
-
-<style>
-
-</style>
