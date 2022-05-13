85952f392a52e74200ea1e5c0c0f4bebeb0b5166
Diego Bustamante <Diego Bustamante>
2022-05-13 01:57:41 -0400

    Se corrige publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
     publicPath: '',
-})
+}
